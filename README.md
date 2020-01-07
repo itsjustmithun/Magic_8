710cbd78367e25824d7c43c02551094bbcd8c946
itsjustmithun <mitbusiness15@gmail.com>
2020-01-07 15:24:54 +0530

    working code

---
+++ README.md
@@ -0,0 +1,17 @@
+# Magic 8 Ball 🎱
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+[![Language: Dart](https://img.shields.io/badge/Language-Dart-blueviolet.svg)](https://dart.dev)
+[![IDE: Android Studio](https://img.shields.io/badge/IDE-Android%20Studio%203.4-green.svg)](https://developer.android.com/studio)
+
+## Description
+A magical 8 ball app, it will give you the answers to all the tricky questions in life.
+
+## Demo
+| Demo |
+| ---- |
+| ![demo.gif](.screenshots/demo.gif) |
+
+## Other Platforms
+| iOS | Android |
+| ------- | ------- |
+| [![apple_logo.png](.screenshots/apple_logo.png)](https://github.com/fdorado985/magic-8-ball-ios) | [![android_logo.png](.screenshots/android_logo.png)](https://github.com/fdorado985/magic-8-ball-android) |
---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+      MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text('Magic 8 Ball 🎱'),
+            backgroundColor: Colors.blue[900],
+          ),
+          backgroundColor: Colors.blue,
+          body: BallPage(),
+        ),
+      ),
+    );
+
+class BallPage extends StatefulWidget {
+  @override
+  _BallPageState createState() => _BallPageState();
+}
+
+class _BallPageState extends State<BallPage> {
+  int ballNumber = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: FlatButton(
+          child: Image.asset('images/ball$ballNumber.png'),
+          onPressed: () {
+            randomizeBall();
+          },
+        ),
+      ),
+    );
+  }
+
+  void randomizeBall() {
+    setState(() {
+      ballNumber = Random().nextInt(5) + 1;
+    });
+  }
+}
